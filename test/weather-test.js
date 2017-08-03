be752c129f1ee0d17ba2fadb405a449d6f53ecd8
Uma Sudhan P <umasudhan.p@gmail.com>
2017-08-04 07:23:52 +1000

    Minor changes- test skeleton for weather route

--- test/weather-test.js
+++ test/weather-test.js
@@ -1,9 +1,31 @@
 const sinon = require('sinon');
 const expect = require('chai').expect;
 const assert = require('chai').assert;
+const needle = require('needle');
 
+const config = require('../config');
 const weather = require('../lib/weather');
-
 describe('Weather information retrieval', function () {
-
+  it('should retrieve weather info description if key, city and country are correct', function(){
+      sinon.stub(needle, "get", function(){
+          return {
+              "weather": [
+                  {
+                      "description": "shower rain",
+                  }
+              ]
+          }
+      });
+      var req = {
+        query:{
+            city:''
+        },
+          country:''
+      };
+      var res = {send(){}};
+      var send = sinon.spy(res, "send");
+      // sinon.spy(weather, "_getOpenWeatherUrl");
+      weather.getWeatherInfo(req, res);
+      // expect(send.getCall(0).args[0]).to.equal('Output:');
+  });
 });
